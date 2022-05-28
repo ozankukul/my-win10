0c0aa0a28507a5b5149e11c35d72264f6608813e
Ozan Kukul <ozankukul@hotmail.com>
2022-05-28 23:30:33 +0300

    Updated main.ps1
    Now runs capslock-task
    Updated TODO

--- main.ps1
+++ main.ps1
@@ -11,15 +11,17 @@ Refresh-Environment;
 
 ./subscripts/initiative/npm.ps1;
 Refresh-Environment;
-
 ./subscripts/initiative/pnpm.ps1;
+
+./subscripts/initiative/capslock-task.ps1;
 
 Write-Host "end of main.ps1";
 
 
 #TODO
-#lively, rustup, set-task
-#PAD configure windows settings, powershell, startup, fileexp quick access, 
-##git, autohotkey, MSVS, notepads, install lively
+#install rustup
+#power-auto system: configure windows settings, startup, fileexp quick access
+#power-auto apps: MSVS, notepads, lively
+#dotfiles: gitconfig, autohotkey script 
 
 #TODO: powershell config script
