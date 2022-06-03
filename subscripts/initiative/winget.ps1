7c41dc70b617ab96a74d130fb031a098e1752bb1
Ozan Kukul <ozankukul@hotmail.com>
2022-06-03 22:35:43 +0300

    Refactor winget.ps1

--- subscripts/initiative/winget.ps1
+++ subscripts/initiative/winget.ps1
@@ -4,28 +4,20 @@ Write-Host "> winget source update"
 winget source update
 
 #TODO accept msstore agreement
+$list = 
+"RARLab.WinRAR",
+"Git.Git",
+"Lexikos.Autohotkey",
+"Google.Chrome",
+"Telegram.TelegramDesktop",
+"Discord.Discord",
+"OpenJS.NodeJS.LTS"
 
-Write-Host "> winget install RARLab.WinRAR --silent"
-winget install RARLab.WinRAR --silent
-
-Write-Host "> winget install Git.Git --silent"
-winget install Git.Git --silent
-
-Write-Host "> winget install Lexikos.Autohotkey --silent"
-winget install Lexikos.Autohotkey --silent 
-
-Write-Host "> winget install Telegram.TelegramDesktop --silent"
-winget install Telegram.TelegramDesktop --silent
-
-Write-Host "> winget install Discord.Discord --silent"
-winget install Discord.Discord --silent
-
-Write-Host "> winget install Microsoft.VisualStudioCode --silent"
-winget install Microsoft.VisualStudioCode --silent
-
-Write-Host "> winget install OpenJS.NodeJS.LTS --silent"
-winget install OpenJS.NodeJS.LTS --silent
-
-Write-Host "end of winget.ps1"
+$list | ForEach-Object {
+    Write-Host "Installing $_" -ForegroundColor Cyan
+    winget install $_ --silent
+}
 
 Refresh-Environment
+
+Write-Host "end of winget.ps1"
