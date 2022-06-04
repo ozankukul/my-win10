096e24231c32edeec80de920576e2c1bfd3cb694
Ozan Kukul <ozankukul@hotmail.com>
2022-06-04 20:21:30 +0300

    Rename folder initiative to main

--- subscripts/initiative/npm.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "> npm.ps1"
-
-Write-Host "> npm install --global pnpm"
-npm install --global pnpm
-
-Write-Host "end of npm.ps1"
--- subscripts/initiative/wsl.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "> wsl.ps1"
-
-Write-Host "> wsl --install"
-wsl --install
-
-Write-Host "end of wsl.ps1"
