e55a29458bd226ec930a5ae562527fd426ec6258
Ozan Kukul <ozankukul@hotmail.com>
2022-06-04 20:28:29 +0300

    Refactor manual.ps1

--- manual.ps1
+++ manual.ps1
@@ -1,19 +1,13 @@
-Write-Host "> manual.ps1" -ForegroundColor magenta
-
 #TODO problematic msvs:
 Write-Host "> winget install Microsoft.VisualStudio.2022.Community --silent"
 winget install Microsoft.VisualStudio.2022.Community --silent
 
-$list = 
-#TODO prompts user
+#TODO prevent user prompts or auto-accept user prompts
 "9NTM2QC6QWS7", #lively wall paper
 "9PF4KZ2VN4W9", #translucenttb
 "9NHL4NSC67WM", #notepads
-"Kingsoft.WPSOffice"
+"Kingsoft.WPSOffice" |
 
-$list | ForEach-Object {
-    Write-Host "Installing $_" -ForegroundColor Cyan
+ForEach-Object {
     winget install $_ --silent
 }
-
-Write-Host "end of manual.ps1"
